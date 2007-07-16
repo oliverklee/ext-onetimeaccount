4706448f848deee21a315fe551e99e9fc16b3cf0
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-07-16 11:04:29 +0000

    Released version 0.1.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/onetimeaccount/trunk@5966 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "onetimeaccount"
 #
-# Auto generated 22-06-2007 15:34
+# Auto generated 16-07-2007 13:03
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'oliverklee.de',
-	'version' => '0.0.0',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '4.1.0-0.0.0',
@@ -45,7 +45,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'static_info_tables' => '2.0.2-',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"0e9f";s:39:"class.tx_onetimeaccount_configcheck.php";s:4:"dae4";s:21:"ext_conf_template.txt";s:4:"1c02";s:12:"ext_icon.gif";s:4:"f11d";s:17:"ext_localconf.php";s:4:"3499";s:14:"ext_tables.php";s:4:"0660";s:13:"locallang.xml";s:4:"ada6";s:16:"locallang_db.xml";s:4:"02a3";s:14:"doc/manual.sxw";s:4:"0a3d";s:14:"pi1/ce_wiz.gif";s:4:"f11d";s:35:"pi1/class.tx_onetimeaccount_pi1.php";s:4:"6eea";s:43:"pi1/class.tx_onetimeaccount_pi1_wizicon.php";s:4:"662f";s:21:"pi1/flexforms_pi1.xml";s:4:"ce4b";s:17:"pi1/locallang.xml";s:4:"e144";s:26:"pi1/onetimeaccount_pi1.css";s:4:"d104";s:27:"pi1/onetimeaccount_pi1.html";s:4:"3ce9";s:26:"pi1/onetimeaccount_pi1.xml";s:4:"0773";s:20:"pi1/static/setup.txt";s:4:"b92b";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"ffc0";s:39:"class.tx_onetimeaccount_configcheck.php";s:4:"b693";s:21:"ext_conf_template.txt";s:4:"1c02";s:12:"ext_icon.gif";s:4:"f11d";s:17:"ext_localconf.php";s:4:"3499";s:14:"ext_tables.php";s:4:"0660";s:13:"locallang.xml";s:4:"ada6";s:16:"locallang_db.xml";s:4:"02a3";s:14:"doc/manual.sxw";s:4:"f425";s:14:"pi1/ce_wiz.gif";s:4:"f11d";s:35:"pi1/class.tx_onetimeaccount_pi1.php";s:4:"fccb";s:43:"pi1/class.tx_onetimeaccount_pi1_wizicon.php";s:4:"d720";s:21:"pi1/flexforms_pi1.xml";s:4:"ce4b";s:17:"pi1/locallang.xml";s:4:"e144";s:26:"pi1/onetimeaccount_pi1.css";s:4:"d104";s:27:"pi1/onetimeaccount_pi1.html";s:4:"3ce9";s:26:"pi1/onetimeaccount_pi1.xml";s:4:"0773";s:20:"pi1/static/setup.txt";s:4:"b92b";}',
 	'suggests' => array(
 	),
 );
