423c1a76960478fb9b590987397f8a8c061ce134
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-04 00:06:00 +0000

    Released onetimeaccount 0.3.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/onetimeaccount/trunk@15467 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "onetimeaccount"
 #
-# Auto generated 13-07-2008 17:34
+# Auto generated 04-01-2009 01:05
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'oliverklee.de',
-	'version' => '0.2.0',
+	'version' => '0.3.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.2-0.0.0',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'sr_feuser_register' => '2.2.0-',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"a773";s:39:"class.tx_onetimeaccount_configcheck.php";s:4:"ae43";s:21:"ext_conf_template.txt";s:4:"1c02";s:12:"ext_icon.gif";s:4:"27c7";s:17:"ext_localconf.php";s:4:"af4b";s:14:"ext_tables.php";s:4:"8541";s:13:"locallang.xml";s:4:"3623";s:16:"locallang_db.xml";s:4:"b6fc";s:14:"doc/manual.sxw";s:4:"1977";s:14:"pi1/ce_wiz.gif";s:4:"be1b";s:35:"pi1/class.tx_onetimeaccount_pi1.php";s:4:"35cb";s:43:"pi1/class.tx_onetimeaccount_pi1_wizicon.php";s:4:"a58b";s:21:"pi1/flexforms_pi1.xml";s:4:"2b0e";s:17:"pi1/locallang.xml";s:4:"ca0e";s:26:"pi1/onetimeaccount_pi1.css";s:4:"e9f4";s:27:"pi1/onetimeaccount_pi1.html";s:4:"0e66";s:26:"pi1/onetimeaccount_pi1.xml";s:4:"423e";s:24:"pi1/static/constants.txt";s:4:"331d";s:20:"pi1/static/setup.txt";s:4:"f717";}',
+	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"47e8";s:39:"class.tx_onetimeaccount_configcheck.php";s:4:"b419";s:21:"ext_conf_template.txt";s:4:"1c02";s:12:"ext_icon.gif";s:4:"27c7";s:17:"ext_localconf.php";s:4:"af4b";s:14:"ext_tables.php";s:4:"8541";s:13:"locallang.xml";s:4:"3623";s:16:"locallang_db.xml";s:4:"b6fc";s:40:"tests/tx_onetimeaccount_pi1_testcase.php";s:4:"08ed";s:50:"tests/fixtures/class.tx_onetimeaccount_fakePi1.php";s:4:"0ca5";s:14:"doc/manual.sxw";s:4:"61f2";s:14:"pi1/ce_wiz.gif";s:4:"be1b";s:35:"pi1/class.tx_onetimeaccount_pi1.php";s:4:"e2e9";s:43:"pi1/class.tx_onetimeaccount_pi1_wizicon.php";s:4:"08dc";s:21:"pi1/flexforms_pi1.xml";s:4:"2b0e";s:17:"pi1/locallang.xml";s:4:"3ee7";s:26:"pi1/onetimeaccount_pi1.css";s:4:"e9f4";s:27:"pi1/onetimeaccount_pi1.html";s:4:"6e18";s:26:"pi1/onetimeaccount_pi1.xml";s:4:"3a67";s:24:"pi1/static/constants.txt";s:4:"331d";s:20:"pi1/static/setup.txt";s:4:"f717";}',
 	'suggests' => array(
 		'sr_feuser_register' => '2.2.0-',
 	),
