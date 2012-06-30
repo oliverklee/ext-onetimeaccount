07b64030e04b6d37f181c918c7728eeecf732320
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-30 09:50:59 +0000

    [Bug 4472] Clean up the includes and the autoload file, r=benny
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/onetimeaccount/trunk@64074 735d13b6-9817-0410-8766-e36946ffe9aa

--- Tests/Fixtures/FakePi1.php
+++ Tests/Fixtures/FakePi1.php
@@ -22,8 +22,6 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-require_once(t3lib_extMgm::extPath('onetimeaccount') . 'pi1/class.tx_onetimeaccount_pi1.php');
-
 /**
  * Fake version of the plugin "One-time FE account creator" for the
  * "onetimeaccount" extension.
