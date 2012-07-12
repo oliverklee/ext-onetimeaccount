cadf8752c201c9de1cc4a25fa4ea89f7bc6c2782
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-12 16:49:32 +0000

    [Bug 4485] Add TYPO3 maximum version to ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/onetimeaccount/trunk@64432 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '4.5.0-0.0.0',
+			'typo3' => '4.5.0-4.7.99',
 			'ameos_formidable' => '1.0.0-1.9.99',
 			'oelib' => '0.7.62-',
 			'static_info_tables' => '2.1.0-',
@@ -47,7 +47,6 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"c049";s:39:"class.tx_onetimeaccount_configcheck.php";s:4:"ffe0";s:31:"class.tx_onetimeaccount_eid.php";s:4:"c821";s:16:"ext_autoload.php";s:4:"da6a";s:21:"ext_conf_template.txt";s:4:"b12b";s:12:"ext_icon.gif";s:4:"27c7";s:17:"ext_localconf.php";s:4:"6008";s:14:"ext_tables.php";s:4:"e694";s:13:"locallang.xml";s:4:"d2a5";s:16:"locallang_db.xml";s:4:"b6fc";s:17:"Tests/pi1Test.php";s:4:"93a4";s:26:"Tests/Fixtures/FakePi1.php";s:4:"0081";s:39:"doc/T3X_marijo-0_1_0-z-200909252202.t3x";s:4:"dad8";s:14:"doc/manual.sxw";s:4:"9a05";s:14:"pi1/ce_wiz.gif";s:4:"be1b";s:35:"pi1/class.tx_onetimeaccount_pi1.php";s:4:"ea66";s:43:"pi1/class.tx_onetimeaccount_pi1_wizicon.php";s:4:"d328";s:21:"pi1/flexforms_pi1.xml";s:4:"460e";s:17:"pi1/locallang.xml";s:4:"6830";s:26:"pi1/onetimeaccount_pi1.css";s:4:"9a64";s:27:"pi1/onetimeaccount_pi1.html";s:4:"8ee0";s:24:"pi1/static/constants.txt";s:4:"331d";s:20:"pi1/static/setup.txt";s:4:"f1e1";}',
 	'suggests' => array(
-		'sr_feuser_register' => '2.2.0-',
 	),
 );
 
