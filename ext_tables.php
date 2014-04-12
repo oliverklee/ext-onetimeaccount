a3545d6958f1a9e5dedbc7b9ebbc592d417ab3ad
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-04-12 10:30:18 +0000

    [#5008] Fix: Call loadTCA only for TYPO3 CMS < 6.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/onetimeaccount/trunk@84787 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,14 +3,18 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-t3lib_div::loadTCA('tt_content');
+if (t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version) < 6001000) {
+	t3lib_div::loadTCA('tt_content');
+}
 
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1']
 	= 'layout,select_key,pages,recursive';
 
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
 
-t3lib_div::loadTCA('fe_users');
+if (t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version) < 6001000) {
+	t3lib_div::loadTCA('fe_users');
+}
 
 $TCA['fe_users']['columns']['company']['config'] = array(
 	'type' => 'text',
