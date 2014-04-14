2d674cb812b1e39416e0aad2ade2546d15f5851b
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-04-14 20:12:39 +0000

    Bump the version number to 0.9.50.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/onetimeaccount/trunk@84824 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "onetimeaccount".
  *
- * Auto generated 15-01-2014 17:39
+ * Auto generated 14-04-2014 20:12
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'oliverklee.de',
-	'version' => '0.9.0',
+	'version' => '0.9.50',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-5.5.99',
@@ -45,7 +45,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'sr_feuser_register' => '2.2.0-',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"fadd";s:39:"class.tx_onetimeaccount_configcheck.php";s:4:"6961";s:16:"ext_autoload.php";s:4:"c6f5";s:21:"ext_conf_template.txt";s:4:"b12b";s:12:"ext_icon.gif";s:4:"27c7";s:17:"ext_localconf.php";s:4:"239d";s:14:"ext_tables.php";s:4:"9be4";s:13:"locallang.xml";s:4:"d2a5";s:16:"locallang_db.xml";s:4:"b6fc";s:17:"Tests/pi1Test.php";s:4:"ab92";s:26:"Tests/Fixtures/FakePi1.php";s:4:"3c63";s:14:"doc/manual.sxw";s:4:"1859";s:39:"doc/T3X_marijo-0_1_0-z-200909252202.t3x";s:4:"dad8";s:14:"pi1/ce_wiz.gif";s:4:"be1b";s:35:"pi1/class.tx_onetimeaccount_pi1.php";s:4:"8e2c";s:43:"pi1/class.tx_onetimeaccount_pi1_wizicon.php";s:4:"81f2";s:21:"pi1/flexforms_pi1.xml";s:4:"460e";s:17:"pi1/locallang.xml";s:4:"6830";s:26:"pi1/onetimeaccount_pi1.css";s:4:"7fbe";s:27:"pi1/onetimeaccount_pi1.html";s:4:"8ee0";s:24:"pi1/static/constants.txt";s:4:"331d";s:20:"pi1/static/setup.txt";s:4:"cf8a";}',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"88ac";s:39:"class.tx_onetimeaccount_configcheck.php";s:4:"6961";s:16:"ext_autoload.php";s:4:"c6f5";s:21:"ext_conf_template.txt";s:4:"b12b";s:12:"ext_icon.gif";s:4:"27c7";s:17:"ext_localconf.php";s:4:"239d";s:14:"ext_tables.php";s:4:"0232";s:13:"locallang.xml";s:4:"d2a5";s:16:"locallang_db.xml";s:4:"b6fc";s:17:"Tests/pi1Test.php";s:4:"fe6b";s:26:"Tests/Fixtures/FakePi1.php";s:4:"3c63";s:14:"doc/manual.sxw";s:4:"8fd2";s:39:"doc/T3X_marijo-0_1_0-z-200909252202.t3x";s:4:"dad8";s:14:"pi1/ce_wiz.gif";s:4:"be1b";s:35:"pi1/class.tx_onetimeaccount_pi1.php";s:4:"cbc3";s:43:"pi1/class.tx_onetimeaccount_pi1_wizicon.php";s:4:"81f2";s:21:"pi1/flexforms_pi1.xml";s:4:"460e";s:17:"pi1/locallang.xml";s:4:"6830";s:26:"pi1/onetimeaccount_pi1.css";s:4:"7fbe";s:27:"pi1/onetimeaccount_pi1.html";s:4:"8ee0";s:24:"pi1/static/constants.txt";s:4:"331d";s:20:"pi1/static/setup.txt";s:4:"cf8a";}',
 	'suggests' => array(
 	),
 );
