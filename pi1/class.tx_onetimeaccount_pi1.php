1402abefd9c6e6596ae09de7f50391a80fb2303e
Oliver Klee <typo3-coding@oliverklee.de>
2016-01-09 14:56:33 +0100

    [BUGFIX] #5235 Explicitly provide the configuration check namespaces
    Change-Id: I4117c9a28ac35386943d0768fd9db0c57e245cd2
    Reviewed-on: https://review.typo3.org/45757
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- pi1/class.tx_onetimeaccount_pi1.php
+++ pi1/class.tx_onetimeaccount_pi1.php
@@ -21,7 +21,7 @@
  * @author Oliver Klee <typo3-coding@oliverklee.de>
  * @author Niels Pardon <mail@niels-pardon.de>
  */
-class tx_onetimeaccount_pi1 extends tx_oelib_templatehelper {
+class tx_onetimeaccount_pi1 extends Tx_Oelib_TemplateHelper implements Tx_Oelib_Interface_ConfigurationCheckable {
 	/**
 	 * make the plugin uncached
 	 *
@@ -839,6 +839,16 @@ class tx_onetimeaccount_pi1 extends tx_oelib_templatehelper {
 		}
 
 		t3lib_div::devLog($message, 'onetimeaccount', $severity);
+	}
+
+	/**
+	 * Returns the prefix for the configuration to check, e.g. "plugin.tx_seminars_pi1.".
+	 *
+	 * @return string the namespace prefix, will end with a dot
+	 */
+	public function getTypoScriptNamespace()
+	{
+		return 'plugin.tx_onetimeaccount_pi1.';
 	}
 }
 
