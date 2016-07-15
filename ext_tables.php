b59067e04826257b86faff3ca33062b86ccbc8c1
Oliver Klee <typo3-coding@oliverklee.de>
2016-07-15 14:03:46 +0200

    [CLEANUP] #5726 Move the FlexForms configuration to Configuration/FlexForms
    Also drop the use of $_EXTKEY from ext_tables.php.
    
    Change-Id: Iab3068777a4a381081d95121bb0ebbe1b16c5b73
    Reviewed-on: https://review.typo3.org/48982
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- ext_tables.php
+++ ext_tables.php
@@ -2,26 +2,27 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    $_EXTKEY . '_pi1',
-    'FILE:EXT:onetimeaccount/pi1/flexforms_pi1.xml'
+    'onetimeaccount_pi1',
+    'FILE:EXT:onetimeaccount/Configuration/FlexForms/FlexForms.xml'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-    array(
+    [
         'LLL:EXT:onetimeaccount/locallang_db.xml:tt_content.list_type_pi1',
-        $_EXTKEY . '_pi1',
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'ext_icon.gif',
-    ),
+        'onetimeaccount_pi1',
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('onetimeaccount') . 'ext_icon.gif',
+    ],
     'list_type'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
+    'onetimeaccount',
     'Configuration/TypoScript/',
     'One-time FE account creator'
 );
 
 if (TYPO3_MODE === 'BE') {
     $TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_onetimeaccount_pi1_wizicon']
-        = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'pi1/class.tx_onetimeaccount_pi1_wizicon.php';
+        = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('onetimeaccount')
+            . 'pi1/class.tx_onetimeaccount_pi1_wizicon.php';
 }
