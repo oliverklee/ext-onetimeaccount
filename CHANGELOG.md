992e07bf7832e32554c037d7880d06e874539fd1
Oliver Klee <typo3-coding@oliverklee.de>
2018-04-21 14:00:48 +0200

    [CLEANUP] Drop the incorrect TYPO3 Core license headers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,7 @@ This project adheres to [Semantic Versioning](https://semver.org/).
 ### Deprecated
 
 ### Removed
+- Drop the incorrect TYPO3 Core license headers (#20)
 - remove the example extension (#6)
 
 ### Fixed
--- Tests/Unit/FrontEnd/DefaultControllerTest.php
+++ Tests/Unit/FrontEnd/DefaultControllerTest.php
@@ -1,18 +1,6 @@
 <?php
 namespace OliverKlee\Onetimeaccount\Tests\Unit\FrontEnd;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
 use OliverKlee\Onetimeaccount\Tests\Unit\FrontEnd\Fixtures\FakeDefaultController;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
@@ -20,7 +8,6 @@ use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 /**
  * Test case.
- *
  *
  * @author Oliver Klee <typo3-coding@oliverklee.de>
  */
