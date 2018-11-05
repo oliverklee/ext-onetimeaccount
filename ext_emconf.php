ddb7f0242868d2e40d3dadbb41997292f291be0c
Oliver Klee <typo3-coding@oliverklee.de>
2018-11-05 23:42:23 +0100

    [TASK] Streamline ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,23 +3,8 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'One-time FE account',
     'description' => 'Allow users to create a one-time FE account to which they will be automatically logged in (without having to enter a user name or password).',
-    'category' => 'plugin',
-    'author' => 'Oliver Klee',
-    'author_email' => 'typo3-coding@oliverklee.de',
-    'shy' => '',
-    'dependencies' => 'felogin,static_info_tables,oelib,mkforms',
-    'conflicts' => '',
-    'priority' => '',
-    'module' => '',
-    'state' => 'stable',
-    'internal' => '',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearCacheOnLoad' => 0,
-    'lockType' => '',
-    'author_company' => 'oliverklee.de',
     'version' => '3.0.1',
+    'category' => 'plugin',
     'constraints' => [
         'depends' => [
             'php' => '5.6.0-7.2.99',
@@ -35,7 +20,12 @@ $EM_CONF[$_EXTKEY] = [
             'sr_feuser_register' => '2.2.0-',
         ],
     ],
-    '_md5_values_when_last_written' => '',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'clearCacheOnLoad' => false,
+    'author' => 'Oliver Klee',
+    'author_email' => 'typo3-coding@oliverklee.de',
+    'author_company' => 'oliverklee.de',
     'autoload' => [
         'classmap' => [
             'Classes',
