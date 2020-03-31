31d8fc0951e82603161a4a83d136a950dcbbe44b
Oliver Klee <typo3-coding@oliverklee.de>
2020-03-31 13:33:53 +0200

    [TASK] Prepare the 4.1.0 release (#153)

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'One-time FE account',
     'description' => 'Allow users to create a one-time FE account to which they will be automatically logged in (without having to enter a user name or password).',
-    'version' => '4.0.1',
+    'version' => '4.1.0',
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
