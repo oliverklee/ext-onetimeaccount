354d5d396927b7dc58b93981d6ab28caa5563e9a
Oliver Klee <typo3-coding@oliverklee.de>
2021-03-05 15:40:39 +0100

    [BUGFIX] Relax the dependencies to allow non-Composer installs again
    Some extension do not provide all versions via the TER. Now the
    dependency versions are relaxed so that installs in non-Composer systems
    is possible in TYPO3 8.7 again.

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,9 +10,9 @@ $EM_CONF[$_EXTKEY] = [
             'php' => '7.0.0-7.4.99',
             'typo3' => '8.7.0-9.5.99',
             'felogin' => '8.7.0-9.5.99',
-            'mkforms' => '9.5.4-9.5.99',
+            'mkforms' => '9.5.2-9.5.99',
             'oelib' => '3.2.0-3.99.99',
-            'static_info_tables' => '6.8.0-6.99.99',
+            'static_info_tables' => '6.7.0-6.99.99',
         ],
         'conflicts' => [
             'kb_md5fepw' => '0.0.0-',
