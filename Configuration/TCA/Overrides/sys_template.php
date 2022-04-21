1f80dd8233df8ecaa4ad28db65ab5a9696e9a9cd
Oliver Klee <typo3-coding@oliverklee.de>
2022-04-21 18:33:22 +0200

    [BUGFIX] Drop reference to inexistent TypoScript template

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,9 +2,8 @@
 
 defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('onetimeaccount', 'Configuration/TypoScript', 'Onetimeaccount');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'onetimeaccount',
-    'Configuration/TypoScript/Frontend/',
-    'Onetimeaccount frontend (optional)'
+    'Configuration/TypoScript',
+    'OneTimeAccount'
 );
