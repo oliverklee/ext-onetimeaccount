399fd2edbee02260883cc6ab51f5a25175d3efdb
Oliver Klee <typo3-coding@oliverklee.de>
2022-04-22 14:53:11 +0200

    [TASK] Add an abstract controller (#227)
    This will help avoid code duplication.

--- Classes/Controller/UserWithAutologinController.php
+++ Classes/Controller/UserWithAutologinController.php
@@ -4,17 +4,9 @@ declare(strict_types=1);
 
 namespace OliverKlee\OneTimeAccount\Controller;
 
-use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-
 /**
  * Plugin for creating a front-end user and directly logging it in.
  */
-class UserWithAutologinController extends ActionController
+class UserWithAutologinController extends AbstractUserController
 {
-    /**
-     * Creates the user creation form (which initially is empty).
-     */
-    public function newAction(): void
-    {
-    }
 }
--- Classes/Controller/UserWithoutAutologinController.php
+++ Classes/Controller/UserWithoutAutologinController.php
@@ -4,17 +4,9 @@ declare(strict_types=1);
 
 namespace OliverKlee\OneTimeAccount\Controller;
 
-use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-
 /**
  * Plugin for creating a front-end user, but without the autologin.
  */
-class UserWithoutAutologinController extends ActionController
+class UserWithoutAutologinController extends AbstractUserController
 {
-    /**
-     * Creates the user creation form (which initially is empty).
-     */
-    public function newAction(): void
-    {
-    }
 }
