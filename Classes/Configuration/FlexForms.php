514b8b51c9d6e730f041e816f785c730e0c422fd
Oliver Klee <typo3-coding@oliverklee.de>
2022-04-26 18:34:22 +0200

    [FEATURE] Add a FlexForms configuration for the fields to show
    This configuration does not have any influence on the FE output yet.
    
    Part of #210

---
+++ Classes/Configuration/FlexForms.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Onetimeaccount\Configuration;
+
+/**
+ * This class provides functions for building FlexForms.
+ */
+class FlexForms
+{
+    /**
+     * @var array<int, non-empty-string>
+     */
+    protected const AVAILABLE_FIELDS = [
+        'company',
+        'gender',
+        'fullSalutation',
+        'name',
+        'firstName',
+        'lastName',
+        'title',
+        'address',
+        'zip',
+        'city',
+        'zone',
+        'country',
+        'email',
+        'telephone',
+        'www',
+        'dateOfBirth',
+        'status',
+        'comments',
+        'privacy',
+    ];
+
+    /**
+     * Sets the selectable items for the fields to display in `$configuration`.
+     *
+     * @param array<string, array<string, string>> $configuration
+     */
+    public function buildFields(array &$configuration): void
+    {
+        /** @var array<int, array{0: non-empty-string, 1: non-empty-string}> $items */
+        $items = [];
+        foreach (static::AVAILABLE_FIELDS as $fieldKey) {
+            $label = 'LLL:EXT:onetimeaccount/Resources/Private/Language/locallang.xlf:' . $fieldKey;
+            $items[] = [$label, $fieldKey];
+        }
+
+        $configuration['items'] = $items;
+    }
+}
