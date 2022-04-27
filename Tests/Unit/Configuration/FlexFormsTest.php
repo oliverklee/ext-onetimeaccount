52e1632464b02200b62fb2bedab44134dae890e9
Oliver Klee <typo3-coding@oliverklee.de>
2022-04-27 18:25:16 +0200

    [FEATURE] Add a FlexForms configuration for the fields to show (#239)
    This configuration does not have any influence on the FE output yet.
    
    Part of #210

---
+++ Tests/Unit/Configuration/FlexFormsTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OliverKlee\Onetimeaccount\Tests\Unit\Configuration;
+
+use OliverKlee\Onetimeaccount\Configuration\FlexForms;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/**
+ * @covers \OliverKlee\Onetimeaccount\Configuration\FlexForms
+ */
+final class FlexFormsTest extends UnitTestCase
+{
+    /**
+     * @var non-empty-string
+     */
+    private const LOCALLANG_PREFIX = 'LLL:EXT:onetimeaccount/Resources/Private/Language/locallang.xlf:';
+
+    /**
+     * @var FlexForms
+     *
+     * We can make this property private once we drop support for TYPO3 V9.
+     */
+    protected $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new FlexForms();
+    }
+
+    /**
+     * @return array<string, array<int, non-empty-string>>
+     */
+    public function fieldKeysDataProvider(): array
+    {
+        return [
+            'company' => ['company'],
+            'gender' => ['gender'],
+            'fullSalutation' => ['fullSalutation'],
+            'name' => ['name'],
+            'firstName' => ['firstName'],
+            'lastName' => ['lastName'],
+            'title' => ['title'],
+            'address' => ['address'],
+            'zip' => ['zip'],
+            'city' => ['city'],
+            'zone' => ['zone'],
+            'country' => ['country'],
+            'email' => ['email'],
+            'telephone' => ['telephone'],
+            'www' => ['www'],
+            'dateOfBirth' => ['dateOfBirth'],
+            'status' => ['status'],
+            'comments' => ['comments'],
+            'privacy' => ['privacy'],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @param non-empty-string $fieldKey
+     *
+     * @dataProvider fieldKeysDataProvider
+     */
+    public function buildFieldsCreatesArrayWithLabelsAndFieldKeys(string $fieldKey): void
+    {
+        $configuration = [];
+        $this->subject->buildFields($configuration);
+
+        self::assertArrayHasKey('items', $configuration);
+        $items = $configuration['items'];
+        self::assertIsArray($items);
+        $expected = [self::LOCALLANG_PREFIX . $fieldKey, $fieldKey];
+        self::assertContains($expected, $items);
+    }
+}
