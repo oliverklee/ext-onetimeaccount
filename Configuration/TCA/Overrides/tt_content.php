e9819d540f660115cb0eb8248eb88c399b040916
Oliver Klee <typo3-coding@oliverklee.de>
2022-05-10 18:01:46 +0200

    [TASK] Disable the unfinished plugin without autologin
    We'll make this plugin available again once it is finished
    enough to be usable.
    
    Fixes #217

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -24,24 +24,27 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['onetimeaccou
     'FILE:EXT:onetimeaccount/Configuration/FlexForms/Plugin.xml'
 );
 
-// This makes the plugin selectable in the BE.
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    // extension name, matching the PHP namespace
-    'OliverKlee.Onetimeaccount',
-    // arbitrary, but unique plugin name (not visible in the BE)
-    'WithoutAutologin',
-    // plugin title, as visible in the drop-down in the BE
-    'LLL:EXT:onetimeaccount/Resources/Private/Language/locallang.xlf:plugin.withoutAutologin',
-    // the icon visible in the drop-down in the BE
-    'EXT:onetimeaccount/Resources/Public/Icons/Extension.svg'
-);
+// disabled as this plugin is not finished yet:
+// https://github.com/oliverklee/ext-onetimeaccount/issues/206
 
-// This removes the default controls from the plugin.
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['onetimeaccount_withoutautologin']
-    = 'recursive,select_key,pages';
-// These two commands add the flexform configuration for the plugin.
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['onetimeaccount_withoutautologin'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'onetimeaccount_withoutautologin',
-    'FILE:EXT:onetimeaccount/Configuration/FlexForms/Plugin.xml'
-);
+// // This makes the plugin selectable in the BE.
+// \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+//     // extension name, matching the PHP namespace
+//     'OliverKlee.Onetimeaccount',
+//     // arbitrary, but unique plugin name (not visible in the BE)
+//     'WithoutAutologin',
+//     // plugin title, as visible in the drop-down in the BE
+//     'LLL:EXT:onetimeaccount/Resources/Private/Language/locallang.xlf:plugin.withoutAutologin',
+//     // the icon visible in the drop-down in the BE
+//     'EXT:onetimeaccount/Resources/Public/Icons/Extension.svg'
+// );
+//
+// // This removes the default controls from the plugin.
+// $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['onetimeaccount_withoutautologin']
+//     = 'recursive,select_key,pages';
+// // These two commands add the flexform configuration for the plugin.
+// $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['onetimeaccount_withoutautologin'] = 'pi_flexform';
+// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+//     'onetimeaccount_withoutautologin',
+//     'FILE:EXT:onetimeaccount/Configuration/FlexForms/Plugin.xml'
+// );
