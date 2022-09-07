61574ed9e84b6465fa6f686426fbf6fbae2df336
Oliver Klee <typo3-coding@oliverklee.de>
2022-09-07 12:05:46 +0200

    [CLEANUP] Use asserts for checking things we know are true
    Also make the method naming a bit more consistent.
    
    Fixes #392

--- Tests/Unit/Service/AutologinTest.php
+++ Tests/Unit/Service/AutologinTest.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace OliverKlee\Onetimeaccount\Tests\Unit\Service;
 
-use OliverKlee\FeUserExtraFields\Domain\Model\FrontendUser;
 use OliverKlee\Onetimeaccount\Service\Autologin;
 use TYPO3\CMS\Core\SingletonInterface;
-use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 /**
@@ -41,36 +39,5 @@ final class AutologinTest extends UnitTestCase
     public function isSingleton(): void
     {
         self::assertInstanceOf(SingletonInterface::class, $this->subject);
-    }
-
-    /**
-     * @test
-     */
-    public function createSessionForUserWithoutFrontEndThrowsException(): void
-    {
-        $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('No frontend controller found.');
-        $this->expectExceptionCode(1651593678);
-
-        unset($GLOBALS['TSFE']);
-        $user = new FrontendUser();
-
-        $this->subject->createSessionForUser($user, 'some-password-hash');
-    }
-
-    /**
-     * @test
-     */
-    public function createSessionForUserWithFrontEndWithoutAuthenticationThrowsException(): void
-    {
-        $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('No frontend user authentication found.');
-        $this->expectExceptionCode(1651593718);
-
-        $GLOBALS['TSFE'] = $this->getMockBuilder(TypoScriptFrontendController::class)
-            ->disableOriginalConstructor()->getMock();
-        $user = new FrontendUser();
-
-        $this->subject->createSessionForUser($user, 'some-password-hash');
     }
 }
