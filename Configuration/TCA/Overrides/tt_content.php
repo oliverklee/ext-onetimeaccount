587dbda18327b611138adc4e93f7b74e8792723a
Oliver Klee <typo3-coding@oliverklee.de>
2024-05-16 23:15:20 +0200

    [TASK] Use imports instead of FQCNs (#862)

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,9 +1,12 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 defined('TYPO3') || die();
 
 // This makes the plugin selectable in the BE.
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+ExtensionUtility::registerPlugin(
     // extension name, matching the PHP namespaces (but without the vendor)
     'Onetimeaccount',
     // arbitrary, but unique plugin name (not visible in the BE)
@@ -21,7 +24,7 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['onetimea
 // These two commands add the flexform configuration for the plugin.
 // @phpstan-ignore-next-line We know that this array key exists and is an array.
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['onetimeaccount_withoutautologin'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+ExtensionManagementUtility::addPiFlexFormValue(
     'onetimeaccount_withoutautologin',
     'FILE:EXT:onetimeaccount/Configuration/FlexForms/Plugin.xml'
 );
